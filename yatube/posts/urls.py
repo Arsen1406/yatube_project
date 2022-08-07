59e561f8a13ae66f1c4ef90dbf77cbd0c7208a04
Арсен Григорян <arsen8577711@yandex.ru>
2022-08-07 21:26:28 +0300

    Добавил Views, Urls

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -19,5 +19,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('group/<slug:slug>/', views.group_posts),
+    path('group/', views.group_posts),
+    path('group/<str:slug>/', views.group_posts_detal),
 ]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,4 +1,12 @@
+from typing import Any
 from django.shortcuts import render, HttpResponse
 
-def index(request):
-    return HttpResponse('Hello')
+def index(request) -> HttpResponse:
+    return HttpResponse('Привет. Это наша главная страница. Добро пожаловать мой друг!))')
+
+def group_posts(request) -> HttpResponse:
+    return HttpResponse('Вот здесь будут все ваши и наши блоги')
+
+
+def group_posts_detal(request, slug: str) -> HttpResponse:
+    return HttpResponse(f'А это пост {slug}')
